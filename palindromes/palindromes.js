e153a645ea3fbc0de209313763f70d527ac0cfe1
Miet <miet.vanderheyden@gmail.com>
2021-06-02 15:45:33 -0400

    Completed palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const regexp = /[A-Z]/gi;
+    const cleanStr = str.match(regexp).join('').toLowerCase();
+    const len = cleanStr.length
+    for (let i=0; i<len; i++) {
+        if (cleanStr[i] !== cleanStr[(len-1)-i]) {
+            return false;
+        } 
+    }
+    return true;
 };
 
 module.exports = palindromes;
