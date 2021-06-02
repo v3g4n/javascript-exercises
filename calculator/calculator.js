a7132f115a9e574ddb040f4e70a2007a4a7adade
Miet <miet.vanderheyden@gmail.com>
2021-06-02 14:28:15 -0400

    Completed calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  arr.forEach((num) => total += num);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  total = 1;
+  arr.forEach((num) => total *= num);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
+  return factorial(num - 1) * num;
 };
 
 module.exports = {
